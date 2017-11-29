fb9ed34d90b7c1f1492721d3d96bdaf48e08f989
m.seiji <web.works.ms@gmail.com>
2017-11-29 09:13:36 +0900

    scroll_updeta11/29

--- scroll.js
+++ scroll.js
@@ -1,6 +1,30 @@
 window.addEventListener('load', scroll_ev, false);
 window.addEventListener('resize', scroll_ev, false);
-
+var flag = false;
 function scroll_ev(){
-	console.log("start");
+	
+	function Target(el){
+		this.el = el;
+	}
+	Target.prototype.set = function(){
+		var item = document.querySelector(this.el);
+		var rect = item.getBoundingClientRect();
+		var success = rect.top + window.pageYOffset
+		return success;
+	}
+
+	window.addEventListener('scroll', function(){
+
+		var scroll_h = window.pageYOffset;
+		var items01 = new Target('el');
+		
+		if(scroll_h > items01.set()){
+			if(!flag){
+				action
+				flag = true;
+			}
+		}
+
+	},false)
 }
+
